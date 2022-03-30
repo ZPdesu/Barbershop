9b78f970b046de1f70b0f646c76fd3c643689588
soumik12345 <19soumik.rakshit96@gmail.com>
2022-03-30 13:03:41 +0000

    updated main.py

--- main.py
+++ main.py
@@ -10,18 +10,26 @@ from models.Blending import Blending
 
 def main(args):
     wandb.login()
-    with wandb.init(project=args.wandb_project, entity=args.wandb_entity, job_type="test"):
+    with wandb.init(
+        project=args.wandb_project, entity=args.wandb_entity, job_type="test"
+    ):
 
-        images_artifact = wandb.use_artifact(args.images_artifact, type='dataset')
+        images_artifact = wandb.use_artifact(args.images_artifact, type="dataset")
         images_artifact_dir = images_artifact.download()
 
-        ffhq_model_artifact = wandb.use_artifact(args.ffhq_models_artifact, type='model')
+        ffhq_model_artifact = wandb.use_artifact(
+            args.ffhq_models_artifact, type="model"
+        )
         ffhq_model_artifact_dir = ffhq_model_artifact.download()
         ffhq_model_file = os.path.join(ffhq_model_artifact_dir, "ffhq.pt")
 
-        segmentation_model_artifact = wandb.use_artifact(args.segmentation_models_artifact, type='model')
+        segmentation_model_artifact = wandb.use_artifact(
+            args.segmentation_models_artifact, type="model"
+        )
         segmentation_model_artifact_dir = segmentation_model_artifact.download()
-        segmentation_model_file = os.path.join(segmentation_model_artifact_dir, "seg.pth")
+        segmentation_model_file = os.path.join(
+            segmentation_model_artifact_dir, "seg.pth"
+        )
 
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -35,70 +43,174 @@ def main(args):
         ii2s.invert_images_in_W([*im_set])
         ii2s.invert_images_in_FS([*im_set])
 
-        align = Alignment(args, ffhq_checkpoint_file=ffhq_model_file, segmentation_checkpoint_file=segmentation_model_file).to(device=device)
-        align.align_images(identity_image, structure_image, sign=args.sign, align_more_region=False, smooth=args.smooth)
+        align = Alignment(
+            args,
+            ffhq_checkpoint_file=ffhq_model_file,
+            segmentation_checkpoint_file=segmentation_model_file,
+        ).to(device=device)
+        align.align_images(
+            identity_image,
+            structure_image,
+            sign=args.sign,
+            align_more_region=False,
+            smooth=args.smooth,
+        )
         if structure_image != appearance_image:
-            align.align_images(identity_image, appearance_image, sign=args.sign, align_more_region=False, smooth=args.smooth, save_intermediate=False)
+            align.align_images(
+                identity_image,
+                appearance_image,
+                sign=args.sign,
+                align_more_region=False,
+                smooth=args.smooth,
+                save_intermediate=False,
+            )
 
-        blend = Blending(args, ffhq_checkpoint_file=ffhq_model_file, segmentation_checkpoint_file=segmentation_model_file).to(device=device)
-        blend.blend_images(identity_image, structure_image, appearance_image, sign=args.sign)
+        blend = Blending(
+            args,
+            ffhq_checkpoint_file=ffhq_model_file,
+            segmentation_checkpoint_file=segmentation_model_file,
+        ).to(device=device)
+        blend.blend_images(
+            identity_image, structure_image, appearance_image, sign=args.sign
+        )
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Barbershop')
+    parser = argparse.ArgumentParser(description="Barbershop")
 
     # I/O arguments
-    parser.add_argument('--wandb_project', type=str, default='barbershop', help='WandB Project Name')
-    parser.add_argument('--wandb_entity', type=str, default=None, help='WandB Entity')
-    parser.add_argument('--images_artifact', type=str, default="geekyrakshit/barbershop/II2S-Images:v0", help='WandB Artifact address for II2S Images')
-    parser.add_argument('--ffhq_models_artifact', type=str, default="geekyrakshit/barbershop/ffhq:v0", help='WandB Artifact address for ffhq model')
-    parser.add_argument('--segmentation_models_artifact', type=str, default="geekyrakshit/barbershop/segmentation:v0", help='WandB Artifact address for segmentation model')
-    parser.add_argument('--output_dir', type=str, default='output', help='The directory to save the latent codes and inversion images')
-    parser.add_argument('--identity_image', type=str, default='16.png', help='Identity image')
-    parser.add_argument('--structure_image', type=str, default='15.png', help='Structure image')
-    parser.add_argument('--appearance_image', type=str, default='117.png', help='Appearance image')
-    parser.add_argument('--sign', type=str, default='realistic', help='realistic or fidelity results')
-    parser.add_argument('--smooth', type=int, default=5, help='dilation and erosion parameter')
+    parser.add_argument(
+        "--wandb_project", type=str, default="barbershop", help="WandB Project Name"
+    )
+    parser.add_argument("--wandb_entity", type=str, default=None, help="WandB Entity")
+    parser.add_argument(
+        "--images_artifact",
+        type=str,
+        default="geekyrakshit/barbershop/II2S-Images:v0",
+        help="WandB Artifact address for II2S Images",
+    )
+    parser.add_argument(
+        "--ffhq_models_artifact",
+        type=str,
+        default="geekyrakshit/barbershop/ffhq:v0",
+        help="WandB Artifact address for ffhq model",
+    )
+    parser.add_argument(
+        "--segmentation_models_artifact",
+        type=str,
+        default="geekyrakshit/barbershop/segmentation:v0",
+        help="WandB Artifact address for segmentation model",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="output",
+        help="The directory to save the latent codes and inversion images",
+    )
+    parser.add_argument(
+        "--identity_image", type=str, default="16.png", help="Identity image"
+    )
+    parser.add_argument(
+        "--structure_image", type=str, default="15.png", help="Structure image"
+    )
+    parser.add_argument(
+        "--appearance_image", type=str, default="117.png", help="Appearance image"
+    )
+    parser.add_argument(
+        "--sign", type=str, default="realistic", help="realistic or fidelity results"
+    )
+    parser.add_argument(
+        "--smooth", type=int, default=5, help="dilation and erosion parameter"
+    )
 
     # StyleGAN2 setting
-    parser.add_argument('--size', type=int, default=1024)
+    parser.add_argument("--size", type=int, default=1024)
     # parser.add_argument('--ckpt', type=str, default="pretrained_models/ffhq.pt")
-    parser.add_argument('--channel_multiplier', type=int, default=2)
-    parser.add_argument('--latent', type=int, default=512)
-    parser.add_argument('--n_mlp', type=int, default=8)
+    parser.add_argument("--channel_multiplier", type=int, default=2)
+    parser.add_argument("--latent", type=int, default=512)
+    parser.add_argument("--n_mlp", type=int, default=8)
 
     # Arguments
-    parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--seed', type=int, default=None)
-    parser.add_argument('--tile_latent', action='store_true', help='Whether to forcibly tile the same latent N times')
-    parser.add_argument('--opt_name', type=str, default='adam', help='Optimizer to use in projected gradient descent')
-    parser.add_argument('--learning_rate', type=float, default=0.01, help='Learning rate to use during optimization')
-    parser.add_argument('--lr_schedule', type=str, default='fixed', help='fixed, linear1cycledrop, linear1cycle')
-    parser.add_argument('--save_intermediate', action='store_true',
-                        help='Whether to store and save intermediate HR and LR images during optimization')
-    parser.add_argument('--save_interval', type=int, default=300, help='Latent checkpoint interval')
-    parser.add_argument('--verbose', action='store_true', help='Print loss information')
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument(
+        "--tile_latent",
+        action="store_true",
+        help="Whether to forcibly tile the same latent N times",
+    )
+    parser.add_argument(
+        "--opt_name",
+        type=str,
+        default="adam",
+        help="Optimizer to use in projected gradient descent",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=0.01,
+        help="Learning rate to use during optimization",
+    )
+    parser.add_argument(
+        "--lr_schedule",
+        type=str,
+        default="fixed",
+        help="fixed, linear1cycledrop, linear1cycle",
+    )
+    parser.add_argument(
+        "--save_intermediate",
+        action="store_true",
+        help="Whether to store and save intermediate HR and LR images during optimization",
+    )
+    parser.add_argument(
+        "--save_interval", type=int, default=300, help="Latent checkpoint interval"
+    )
+    parser.add_argument("--verbose", action="store_true", help="Print loss information")
     # parser.add_argument('--seg_ckpt', type=str, default='pretrained_models/seg.pth')
 
     # Embedding loss options
-    parser.add_argument('--percept_lambda', type=float, default=1.0, help='Perceptual loss multiplier factor')
-    parser.add_argument('--l2_lambda', type=float, default=1.0, help='L2 loss multiplier factor')
-    parser.add_argument('--p_norm_lambda', type=float, default=0.001, help='P-norm Regularizer multiplier factor')
-    parser.add_argument('--l_F_lambda', type=float, default=0.1, help='L_F loss multiplier factor')
-    parser.add_argument('--W_steps', type=int, default=1100, help='Number of W space optimization steps')
-    parser.add_argument('--FS_steps', type=int, default=250, help='Number of W space optimization steps')
+    parser.add_argument(
+        "--percept_lambda",
+        type=float,
+        default=1.0,
+        help="Perceptual loss multiplier factor",
+    )
+    parser.add_argument(
+        "--l2_lambda", type=float, default=1.0, help="L2 loss multiplier factor"
+    )
+    parser.add_argument(
+        "--p_norm_lambda",
+        type=float,
+        default=0.001,
+        help="P-norm Regularizer multiplier factor",
+    )
+    parser.add_argument(
+        "--l_F_lambda", type=float, default=0.1, help="L_F loss multiplier factor"
+    )
+    parser.add_argument(
+        "--W_steps", type=int, default=1100, help="Number of W space optimization steps"
+    )
+    parser.add_argument(
+        "--FS_steps", type=int, default=250, help="Number of W space optimization steps"
+    )
 
     # Alignment loss options
-    parser.add_argument('--ce_lambda', type=float, default=1.0, help='cross entropy loss multiplier factor')
-    parser.add_argument('--style_lambda', type=str, default=4e4, help='style loss multiplier factor')
-    parser.add_argument('--align_steps1', type=int, default=140, help='')
-    parser.add_argument('--align_steps2', type=int, default=100, help='')
+    parser.add_argument(
+        "--ce_lambda",
+        type=float,
+        default=1.0,
+        help="cross entropy loss multiplier factor",
+    )
+    parser.add_argument(
+        "--style_lambda", type=str, default=4e4, help="style loss multiplier factor"
+    )
+    parser.add_argument("--align_steps1", type=int, default=140, help="")
+    parser.add_argument("--align_steps2", type=int, default=100, help="")
 
     # Blend loss options
-    parser.add_argument('--face_lambda', type=float, default=1.0, help='')
-    parser.add_argument('--hair_lambda', type=str, default=1.0, help='')
-    parser.add_argument('--blend_steps', type=int, default=400, help='')
+    parser.add_argument("--face_lambda", type=float, default=1.0, help="")
+    parser.add_argument("--hair_lambda", type=str, default=1.0, help="")
+    parser.add_argument("--blend_steps", type=int, default=400, help="")
 
     args = parser.parse_args()
     main(args)
